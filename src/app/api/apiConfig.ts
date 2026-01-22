7c849d53a52ebda7cad7e2bbd8382d370ca2c5d6
Harsh Suthar <hsuthar1125@gmail.com>
2026-01-22 08:26:26 +0530

    Update apiConfig.ts

--- src/app/api/apiConfig.ts
+++ src/app/api/apiConfig.ts
@@ -1,3 +1,5 @@
 // apiConfig.ts
 export const API_BASE_URL = "https://api.hicine.info";
-export const formator = "https://hicine-formator.vercel.app/type";
+
+
+export const formator = "https://hicine-formator-one.vercel.app/type";
